d54863e57837d96916f6d8dc192bd41a5ace73d9
shane <shane.mccully@distilled.ie>
2018-09-21 12:44:10 +0100

    Initial Commit

---
+++ targetsTests/targetsTests.swift
@@ -0,0 +1,36 @@
+//
+//  targetsTests.swift
+//  targetsTests
+//
+//  Created by Shane McCully on 21/09/2018.
+//  Copyright © 2018 Shane McCully. All rights reserved.
+//
+
+import XCTest
+@testable import targets
+
+class targetsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
