a4a91a23d373378108ec189a6a5c910d9e4c12b0
shane <shane.mccully@distilled.ie>
2018-09-24 09:54:59 +0100

    initial commit w/ message

--- targets/ViewController.swift
+++ targets/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import Foundation
+
+
+extension String {
+    static var Empty = ""
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+       // print(Config.endpoint)
+
+        #if targetsOne
+        print("RUNNING TARGETS ONE")
+        #else
+        print("RUNNING TARGETS TWO")
+        #endif
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPrint(_ sender: Any) {
 
+        print(Config.endpoint)
+       // printEndpoint()
+    }
+
+//    func printEndpoint() {
+//
+//        if let path = Bundle.main.path(forResource: "Info", ofType: "plist") {
+//            if let dic = NSDictionary(contentsOfFile: path){
+//               print(dic)
+//            }
+//        }
+//
+//    }
+}
+
+
+
+
+class Config {
+
+    static var endpoint: String {
+        get {
+
+            if let path = Bundle.main.path(forResource: "Info", ofType: "plist") {
+                if let dic = NSDictionary(contentsOfFile: path) {
+                    return dic["ENDPOINT_URL"] as? String ?? String.Empty
+                }
+            }
+
+            return String.Empty
+
+        }
+    }
 }
 
---
+++ targets/targetsOne/targetsClass.swift
@@ -0,0 +1,9 @@
+//
+//  targetsClass.swift
+//  targetsOne
+//
+//  Created by Shane McCully on 21/09/2018.
+//  Copyright © 2018 Shane McCully. All rights reserved.
+//
+
+import Foundation
